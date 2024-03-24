581f5f0407da31c09d665069a94f38954201d476
Daniele <daniele.tortora@frontify.com>
2024-03-24 16:21:12 +0100

    feat: create repl environment

---
+++ repl.ts
@@ -0,0 +1,21 @@
+import { Parser } from "./src/parser/parser"
+
+async function repl() {
+    const parser = new Parser()
+    while (true) {
+        const input = await new Promise<string>((resolve) => {
+            process.stdout.write('Repl@0.1> ')
+            process.stdin.on('data', (data) => {
+                resolve(data.toString())
+            })
+        })
+
+        if (input === 'exit\n') {
+            break
+        }
+
+        console.log(parser.parse(input))
+    }
+}
+
+repl();
